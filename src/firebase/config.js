e73215766b110696323f747476a3eb86c2169e74
vkasyan76 <vkasyan@gmx.de>
2022-10-31 22:14:23 +0100

    Firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCteWgMxvYAdcSINJDFBvCePmZ0RFU83w8',
+    authDomain: 'readinglistapp-ab3d0.firebaseapp.com',
+    projectId: 'readinglistapp-ab3d0',
+    storageBucket: 'readinglistapp-ab3d0.appspot.com',
+    messagingSenderId: '876190991359',
+    appId: '1:876190991359:web:39c23c0396b08898cf9dca',
+}
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init firestore
+const db = getFirestore()
+
+export { db }
