6839eac2d3b317ea7949f3297fec24ed0b70c182
vkasyan76 <vkasyan@gmx.de>
2022-10-31 23:19:22 +0100

    Getting Documents form Firestore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
 import BookList from '../components/BookList'
 import BookForm from '../components/BookForm'
 
+import { db } from '../firebase/config'
+
 export default function Home() {
-  const [books, setBooks] = useState([
-    { title: 'the name of the wind', id: 1 },
-    { title: 'the dragon reborn', id: 2 },
-    { title: 'the final empire', id: 3 },
-    { title: 'the way of kings', id: 4 }
-  ])
+  const [books, setBooks] = useState(null)
+  // const [books, setBooks] = useState([
+  //   // { title: 'the name of the wind', id: 1 },
+  //   // { title: 'the dragon reborn', id: 2 },
+  //   // { title: 'the final empire', id: 3 },
+  //   // { title: 'the way of kings', id: 4 }])
+
+  useEffect(() => {
+    const ref = collection(db, 'books')
+    getDocs(ref).then((snapshot) => {
+      let results = []
+      snapshot.docs.forEach((doc) => {
+        results.push({ id: doc.id, ...doc.data() })
+      })
+      setBooks(results)
+    })
+  }, [])
 
   return (
     <div className="App">
       {books && <BookList books={books} />}
       <BookForm />
     </div>
-  );
+  )
 }
