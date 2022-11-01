7f6adb135cb6ed6071a64789e8cf25d624d9ef38
vkasyan76 <vkasyan@gmx.de>
2022-11-01 23:39:38 +0100

    Getting Real Time Collection Data - useSnapshot

---
+++ src/hooks/useCollection.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import { collection, onSnapshot } from 'firebase/firestore'
+import { db } from '../firebase/config'
+
+export const useCollection = (c) => {
+  const [documents, setDocuments] = useState(null)
+
+  useEffect(() => {
+    let ref = collection(db, c)
+    const unsub = onSnapshot(ref, (snapshot) => {
+      let results = []
+      snapshot.docs.forEach((doc) => {
+        results.push({ id: doc.id, ...doc.data() })
+      })
+      setDocuments(results)
+    })
+
+    return () => unsub()
+  }, [c])
+
+  return { documents }
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,30 @@
-import { useState, useEffect } from 'react'
-import { collection, getDocs } from 'firebase/firestore'
+// import { useState, useEffect } from 'react'
+// import { collection, getDocs } from 'firebase/firestore'
 import BookList from '../components/BookList'
 import BookForm from '../components/BookForm'
+import { useCollection } from '../hooks/useCollection'
 
-import { db } from '../firebase/config'
+// import { db } from '../firebase/config'
 
 export default function Home() {
-  const [books, setBooks] = useState(null)
-  // const [books, setBooks] = useState([
-  //   // { title: 'the name of the wind', id: 1 },
-  //   // { title: 'the dragon reborn', id: 2 },
-  //   // { title: 'the final empire', id: 3 },
-  //   // { title: 'the way of kings', id: 4 }])
+  const { documents: books } = useCollection('books')
+  // const [books, setBooks] = useState(null)
+  // // const [books, setBooks] = useState([
+  // //   // { title: 'the name of the wind', id: 1 },
+  // //   // { title: 'the dragon reborn', id: 2 },
+  // //   // { title: 'the final empire', id: 3 },
+  // //   // { title: 'the way of kings', id: 4 }])
 
-  useEffect(() => {
-    const ref = collection(db, 'books')
-    getDocs(ref).then((snapshot) => {
-      let results = []
-      snapshot.docs.forEach((doc) => {
-        results.push({ id: doc.id, ...doc.data() })
-      })
-      setBooks(results)
-    })
-  }, [])
+  // useEffect(() => {
+  //   const ref = collection(db, 'books')
+  //   getDocs(ref).then((snapshot) => {
+  //     let results = []
+  //     snapshot.docs.forEach((doc) => {
+  //       results.push({ id: doc.id, ...doc.data() })
+  //     })
+  //     setBooks(results)
+  //   })
+  // }, [])
 
   return (
     <div className="App">
