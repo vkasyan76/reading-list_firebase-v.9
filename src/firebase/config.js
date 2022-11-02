827bcf087926536b334154e466e5a090c46e6216
vkasyan76 <vkasyan@gmx.de>
2022-11-03 00:29:04 +0100

    Setting Firebase Auth & Signin Users

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: 'AIzaSyCteWgMxvYAdcSINJDFBvCePmZ0RFU83w8',
@@ -16,4 +17,7 @@ initializeApp(firebaseConfig)
 // init firestore
 const db = getFirestore()
 
-export { db }
+// init firebase auth
+const auth = getAuth()
+
+export { db, auth }
