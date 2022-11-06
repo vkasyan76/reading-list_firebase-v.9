78c033e2d336366123430eba85278e8f95859b00
vkasyan76 <vkasyan@gmx.de>
2022-11-06 12:25:29 +0100

    Dispatching Actions

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import { auth } from '../firebase/config'
 import { signInWithEmailAndPassword } from 'firebase/auth'
+import { useAuthContext } from './useAuthContext'
 
 export const useLogin = () => {
   const [error, setError] = useState(null)
+
+  const { dispatch } = useAuthContext()
 
   const login = (email, password) => {
     setError(null)
     signInWithEmailAndPassword(auth, email, password)
       .then((res) => {
-        console.log('user logged in:', res.user)
+        // console.log('user logged in:', res.user)
+        dispatch({ type: 'LOGIN', payload: res.user })
       })
       .catch((err) => {
         setError(err.message)
--- src/hooks/useSignup.jsx
+++ src/hooks/useSignup.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import { auth } from '../firebase/config'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
   const [error, setError] = useState(null)
+
+  const { dispatch } = useAuthContext()
 
   const signup = (email, password) => {
     setError(null)
     createUserWithEmailAndPassword(auth, email, password)
       .then((res) => {
-        console.log('user signed up:', res.user)
+        // console.log('user signed up:', res.user)
+        dispatch({ type: 'LOGIN', payload: res.user })
       })
       .catch((err) => {
         setError(err.message)
